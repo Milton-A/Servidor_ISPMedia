ce8501886b6e060fa807ae027cd0cdadf4225c32
Oivlisfriend <robertomarcelogongo@gmail.com>
2024-06-30 22:55:27 +0200

    feat: added route to show  playlist

--- src/routes/criticas/routes.ts
+++ src/routes/criticas/routes.ts
@@ -7,7 +7,7 @@ router.get("/", CriticaController.list);
 router.post("/create", CriticaController.create);
 router.put("/update/:id", CriticaController.update);
 router.get("/getGroup/:id", CriticaController.findById);
-router.get("/getCommentByIdMidia/:id", CriticaController.getCommentByIdMidia);
+router.get("/getMidiaById/:id", CriticaController.getCommentByIdMidia);
 router.delete("/delete/:id", CriticaController.delete);
 
 export default router;
