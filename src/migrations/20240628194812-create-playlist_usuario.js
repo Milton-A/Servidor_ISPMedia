a23ee0b20e815ec059cf9acaa09f486887f85e26
Milton-A <miltondantas33@outlook.com>
2024-07-10 02:35:20 +0100

    bd error

--- src/migrations/20240628194812-create-playlist_usuario.js
+++ src/migrations/20240628194812-create-playlist_usuario.js
@@ -28,6 +28,29 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
     });
+    await queryInterface.addConstraint('playlist_usuario',
+      {
+        fields: ['id_perfil_usuario'],
+        type: 'foreign key',
+        name: 'fk_playlist_media_id_perfil_usuario',
+        references: {
+          table: 'perfil_usuario',
+          field: 'id_perfil_usuario',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      });
+    await queryInterface.addConstraint('playlist_usuario', {
+      fields: ['id_playlist'],
+      type: 'foreign key',
+      name: 'fk_pla_id_playlist',
+      references: {
+        table: 'playlist',
+        field: 'id_playlist',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
   },
 
   async down(queryInterface, Sequelize) {
